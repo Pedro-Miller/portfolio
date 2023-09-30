f8e1f05fc6f8fee4cb5855ce0b44acf77bffe85e
Pedro Miller <ph19miller@gmail.com>
2023-09-30 02:07:02 -0300

    beggining main page

---
+++ src/index-functions.js
@@ -0,0 +1,111 @@
+// DOM getters
+const themeToggle = document.querySelectorAll(".index-theme-toggle")
+const moon = document.querySelectorAll('.index-moon')
+const body = document.querySelector('body')
+const navbar = document.getElementById('index-navbar')
+const navbarLink = document.querySelectorAll('.index-navbar-link')
+const theme = localStorage.getItem('theme')
+const ourWork = document.getElementById('index-dropdown-ourwork')
+const dropdown = document.querySelector('#index-dropdown')
+const mobileMenuButton = document.querySelector('#index-mobile-menu')
+const mobileMenu = document.querySelector('#index-dropdown-mobile')
+const dropdownMobile = document.querySelectorAll('.index-dropdown-list-mobile')
+const ourWorkMobile = document.querySelector('#index-dropdown-ourwork-mobile')
+let width = screen.width
+
+
+// Mobile menu computed style getters
+const mobileMenuStyleGet = window.getComputedStyle(mobileMenu)
+const ourWorkMobileStyleGet = window.getComputedStyle(dropdownMobile[0])
+
+
+export function eventListenerHandler(){
+    ourWork.addEventListener('mouseenter', dropdownourWork)
+    ourWork.addEventListener('mouseleave', dropupourWork)
+    dropdown.addEventListener('mouseenter', dropdownourWork)
+    dropdown.addEventListener('mouseleave', dropupourWork)
+    mobileMenuButton.addEventListener("touchstart", openMenu)
+    ourWorkMobile.addEventListener("touchstart", dropdownourWorkMobile)
+}
+
+
+export function darkModeGet(){
+    if(theme == 'darkmode')
+    {
+        body.classList.toggle('index-darkmode')
+        mobileMenuButton.style.filter = 'invert(0)'
+    }
+    else{
+        mobileMenuButton.style.filter = 'invert(100)'
+    }
+}
+
+export function scrollHandler(){
+    window.onscroll = function() {scrollFunction()};
+    function scrollFunction() {
+        if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+            navbar.style.backgroundColor = 'var(--clr-dark)'
+            navbarLink.forEach(link =>{link.style.color = 'var(--clr-light)'})
+            moon.forEach(moon => {moon.style.filter = 'invert(0)'})
+        } else {
+            navbar.style.backgroundColor = 'transparent'
+            navbarLink.forEach(link =>{link.style.color = 'aliceblue'})
+            moon.forEach(moon => {moon.style.filter = 'invert(100)'})
+        }
+    }
+}
+
+
+function dropdownourWork(){
+    dropdown.style.display = 'flex'
+}
+function dropupourWork(){
+    dropdown.style.display = 'none'
+}
+
+function dropdownourWorkMobile(){
+    let ourWorkMobileDisplay = ourWorkMobileStyleGet.getPropertyValue('display') 
+    if(ourWorkMobileDisplay != 'none'){
+        dropdownMobile.forEach(item =>{item.style.display = 'none'})
+    }
+    else{
+        dropdownMobile.forEach(item =>{item.style.display = 'flex'})
+    }
+}
+
+function openMenu(){
+    let mobileMenuDisplay = mobileMenuStyleGet.getPropertyValue('display')
+    if(mobileMenuDisplay != 'none'){
+        mobileMenu.style.display = 'none'
+        dropdownMobile.forEach(item =>{item.style.display = 'none'})
+    }
+    else{
+        mobileMenu.style.display = 'flex'
+    }
+}
+
+export function themeToggleHandler(){
+    themeToggle.forEach(btn =>{btn.addEventListener('click', ()=>{
+    body.classList.toggle('index-darkmode')
+    if(body.classList.contains('index-darkmode'))
+    {
+        localStorage.setItem('theme', 'darkmode')
+        mobileMenuButton.style.filter = 'invert(0)'
+        
+    }
+    else
+    {
+        localStorage.removeItem('theme')
+        mobileMenuButton.style.filter = 'invert(100)'
+    }
+    })})
+}
+
+
+
+
+
+
+
+
+
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import * as idx from './index-functions.js'
+
+window.onload= function(){
+    idx.darkModeGet()
+    idx.eventListenerHandler()
+    idx.themeToggleHandler()
+    idx.scrollHandler()
+}
